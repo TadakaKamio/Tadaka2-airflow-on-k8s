abeefb2273250f1c4e327e4c45c3deebdbf567e5
TadakaKamio <55869030+TadakaKamio@users.noreply.github.com>
2025-07-06 20:59:18 +0900

    Create ud_file_get.py

---
+++ example_dags/ud2/ud_file_get.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append("/usr/local/airflow/dags/gitdags/exapmle_dags")
+
+from common.log import MyAppLog
+
+def main():
+    # Call the imported function
+    MyAppLog()
+    print("Main program execution completed")
+
+if __name__ == "__main__":
+    main()
+  
